50cfdc13974d510b8a4c6416a4906ffcc8c6bf02
Moakarlssonns <mo-karlsson@hotmail.com>
2025-12-11 22:27:06 +0100

    Queries to create Database

---
+++ src/db/create.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS lab_01;
+CREATE DATABASE lab_01;
+USE lab_01;
+
+CREATE TABLE supplier (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    contact_info VARCHAR(50)
+);
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    quantity INT,
+    price FLOAT, 
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    supplier_id INT,
+    FOREIGN KEY(supplier_id) REFERENCES supplier(id)
+);
+
+INSERT INTO supplier (name, contact_info) VALUES
+("Samsung", "samsung@icloud.com"),
+("Apple", "apple@contact.com"),
+("Lindex", "lindex@hotmail.com"),
+("Hm", "hm@info.com"),
+("Starstable", "starstable@mail.com");
+
+INSERT INTO products (name, quantity, price, supplier_id) VALUES 
+("Samsung galaxy mini", 100, 3000, 1),
+("Iphone 16 PRO", 50, 13000, 2),
+("Cardigan", 20, 299.99, 3),
+("Horse t-shirt", 5, 150, 5);
+
+SHOW TABLES;
+SELECT * FROM products;
+SELECT * FROM supplier;
