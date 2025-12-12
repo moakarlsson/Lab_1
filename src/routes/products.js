452255e304b768ea007d2f35967ffccd3212dcee
Moakarlssonns <mo-karlsson@hotmail.com>
2025-12-12 19:35:08 +0100

    feat: add routes for retrieving supplier and inventory information by product ID

--- src/routes/products.js
+++ src/routes/products.js
@@ -8,10 +8,10 @@ router.get("/", async (req, res) => {
     if (req.query.max === undefined || isNaN(maxQuantity)) {
         try {
             const [rows] = await db.query("SELECT * FROM products");
-            res.status(200).json(rows);
+            return res.status(200).json(rows);
         } catch (error) {
             console.error(error);
-            res.status(500).json({error: "Internal server error"});
+            return res.status(500).json({error: "Internal server error"});
         }
     }
     try {
@@ -36,6 +36,53 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+router.get("/:product_id/supplier", async(req, res) => {
+    const productId = parseInt(req.params.product_id,10);
+    if (isNaN(productId)) {
+        return res.status(400).json({error: "Invalid product Id"});
+    }
+    try {
+        const [rows] = await db.query(
+            `SELECT s.id AS supplier_id, s.name AS supplier_name, s.contact_info 
+             FROM products p 
+             JOIN supplier s ON p.supplier_id = s.id 
+             WHERE p.id = ?`,
+            [productId]
+        );
+        if (rows.length === 0) {
+            return res.status(404).json({error: "Supplier not found"});
+        }
+        res.status(200).json(rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "Internal server error"});
+    }
+});
+
+router.get("/:product_id/inventory", async(req,res) => {
+    const productId = parseInt(req.params.product_id, 10);
+
+    if (isNaN(productId)) {
+        return res.status(400).json({ error: "Invalid product ID" });
+    }
+    try {
+        const [rows] = await db.query(
+            `SELECT id AS product_id, supplier_id, quantity, createdAt AS last_updated 
+             FROM products 
+             WHERE id = ?`,
+            [productId]
+        );
+        if (rows.length === 0) {
+            return res.status(404).json({error: "Product not found"});
+        }
+        res.status(200).json(rows[0]); 
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "Internal server error"});   
+    }
+    
+
+});
 
 router.post("/", async (req,res) => {
     const  { name, quantity, price, supplier_id } = req.body;
