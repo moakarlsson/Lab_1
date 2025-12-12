eb8006716eba8944d4973d5cdd6e5b99fe6d25b4
Moakarlssonns <mo-karlsson@hotmail.com>
2025-12-12 15:06:24 +0100

    feat: add filtering for products based on maximum quantity in GET route

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,9 +4,19 @@ const router = express.Router();
 
 
 router.get("/", async (req, res) => {
+    const maxQuantity = parseInt(req.query.max, 10);
+    if (req.query.max === undefined || isNaN(maxQuantity)) {
+        try {
+            const [rows] = await db.query("SELECT * FROM products");
+            res.status(200).json(rows);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({error: "Internal server error"});
+        }
+    }
     try {
-        const [rows] = await db.query("SELECT * FROM products");
-        res.status(200).json(rows);
+        const [filtered] = await db.query("SELECT * FROM products WHERE quantity < ?", [maxQuantity]);
+        res.status(200).json(filtered); 
     } catch (error) {
         console.error(error);
         res.status(500).json({error: "Internal server error"});
@@ -25,6 +35,7 @@ router.get("/:id", async (req, res) => {
         res.status(500).json({error: "Internal server error"});
     }
 });
+
 
 router.post("/", async (req,res) => {
     const  { name, quantity, price, supplier_id } = req.body;
